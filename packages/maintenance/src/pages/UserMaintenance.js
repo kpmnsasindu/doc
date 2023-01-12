058f7a3c8687fb84257d472bf9a9b379735309ba
Sasindu Madushan <sasindu@dmsswe.com>
2023-01-13 01:57:56 +0530

    change string

--- packages/maintenance/src/pages/UserMaintenance.js
+++ packages/maintenance/src/pages/UserMaintenance.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function UserMaintenance() {
-	return <div>User Maintenance from remote</div>;
+	return <div>User Maintenance from remote second check</div>;
 }
