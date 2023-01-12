8146219821041c5516f28cabe34eeeb7db36de0c
Sasindu Madushan <sasindu@dmsswe.com>
2023-01-12 23:23:23 +0530

    public path added to the maintenance

--- packages/maintenance/config/webpack.prod.js
+++ packages/maintenance/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
 	mode: 'production',
 	output: {
 		filename: '[name].[contenthash].js',
-		publicPath: '/container/latest/',
+		publicPath: '/maintenance/latest/',
 	},
 	plugins: [
 		new ModuleFederationPlugin({
